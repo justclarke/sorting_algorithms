0735577161e41d667fd0c5784ffd79b8d755d39e
Robert Clarke <robertclarke29.rc@gmail.com>
2023-10-24 08:21:51 -0700

    update

--- sort.h
+++ sort.h
@@ -21,8 +21,8 @@ typedef struct listint_s { const int n; struct listint_s *prev; struct
 	listint_s *next; } listint_t;
 
 /* Printing helper functions */
-void print_array(const int *array, size_t size); void print_list(const
-		listint_t *list);
+void print_array(const int *array, size_t size); 
+void print_array(const int *array, size_t size)
 
 /* Sorting algoritms */
 void bubble_sort(int *array, size_t size); void insertion_sort_list(listint_t
